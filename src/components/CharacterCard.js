4e77a14fa7e21faac845486508fc1c14b56f7629
floraw <flora.e.winters@gmail.com>
2020-01-26 17:48:10 -0800

    phase 1 complete

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+
+const CharacterCard = (props) => {
+  const [character, setCharacter] = useState();
+
+  useEffect(() => {
+    const id = props.match.params.id;
+
+    axios
+    .get(`https://rickandmortyapi.com/api/character/${id}`)
+    .then(response => {
+      setCharacter(response.data);
+    })
+    .catch(error => {
+      console.log("Error:", error);
+    });
+
+  },[props.match.params.id])
+
+  if(!character) {
+    return <div>Loading character...</div>
+  }
+
+  console.log(character);
+
+  const { name, status, species, image, episode, origin } = character;
+  return (
+    <div className="character-card">
+      <div className="image-wrapper">
+        <img src={image} />
+      </div>
+      <div className="character-details">
+        <h2>{name}</h2>
+        <h4>Starring in {episode.length} episodes!</h4>
+        <p>A {species} from planet {origin.name}, currently {status}</p>
+      </div>
+    </div>
+  )
 }
+
+export default CharacterCard;
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,55 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
-export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+const CharacterList = props => {
+  
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
+
+    const getCharacters = () => {
+      axios
+      .get("https://rickandmortyapi.com/api/character/")
+      .then(res => {
+        setCharacters(res.data.results);
+      })
+      .catch(error => {
+        console.log("Error: ", error);
+      });
+    }
+
+    getCharacters();
+
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
   }, []);
 
+  console.log(characters);
+
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {characters.map(character => (
+        <div>
+          <Link to={`/characters/${character.id}`}>
+          <CharacterDetails key={character.id} character={character} />
+          </Link>
+        </div>
+      ))}
     </section>
   );
 }
+
+function CharacterDetails({ character }) {
+  const { image, name, status, origin } = character;
+
+  return (
+    <div>
+      <img src={image} />
+      <h2>{name}</h2>
+      <p>Status: {status} | From: {origin.name} </p>
+    </div>
+  )
+};
+
+export default CharacterList;
