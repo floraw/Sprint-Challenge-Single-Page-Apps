a994b591b744a6e5d639d7e825a34ec3c984c7b4
floraw <flora.e.winters@gmail.com>
2020-01-26 18:57:26 -0800

    MVP

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,62 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 export default function SearchForm() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  let search = "";
+
+  //add input, onSubmit the input is set to state searchTerm
+
+  //make axios call with parameter that includes searchTerm
+
+  //display the names as a ul
+
+  //need a submit handler
+
+  const changeHandler = event => {
+    setSearchTerm(event.target.value);
+  }
+
+  const submitHandler = event => {
+    event.preventDefault();
+    search += searchTerm;
+    console.log("I searched", search);
+    axios
+    .get(`https://rickandmortyapi.com/api/character/?name=${search}`)
+    .then(res => {
+      setSearchResults(res.data.results);
+      console.log(res.data.results);
+    })
+    .catch(error => {
+      console.log("Error:", error)
+    })
+  }
+
+ 
+  
+
  
   return (
     <section className="search-form">
-     // Add a search form here
+      <form onSubmit={submitHandler}>
+        <label htmlFor="name">Search by Name:</label>
+        <input 
+          name="name"
+          type="text"
+          value={searchTerm}
+          onChange={changeHandler}
+        />
+        <button type="submit">Search</button>
+      </form>
+      <div className="search-results">
+        <ul>
+          {searchResults.map(person => (
+            <li>{person.name}</li>
+          ))}
+        </ul>
+      </div>
+     
     </section>
   );
 }
